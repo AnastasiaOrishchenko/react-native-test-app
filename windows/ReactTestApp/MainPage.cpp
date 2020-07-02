d60b893524dd18c9d62cbbf12536ecc0b10ed74d
Anastasia Orishchenko <t-anoris@microsoft.com>
2020-07-02 11:25:26 +0200

    Load components from manifest

--- windows/ReactTestApp/MainPage.cpp
+++ windows/ReactTestApp/MainPage.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "MainPage.h"
+#include "Manifest.h"
+#include "ComponentViewModel.h"
 #include "MainPage.g.cpp"
 
 using namespace winrt;
@@ -10,5 +12,20 @@ namespace winrt::ReactTestApp::implementation
     MainPage::MainPage()
     {
         InitializeComponent();
+    }
+
+    Windows::Foundation::Collections::IVector<ReactTestApp::ComponentViewModel> MainPage::Components()
+    {
+        Windows::Foundation::Collections::IVector<ReactTestApp::ComponentViewModel> components =
+            winrt::single_threaded_observable_vector<ReactTestApp::ComponentViewModel>();
+
+        for (auto &&c : ManifestProvider::getManifest().components) {
+           hstring componentName = to_hstring(c.displayName.value_or(c.appKey));
+            ReactTestApp::ComponentViewModel newComponent =
+                winrt::make<ReactTestApp::implementation::ComponentViewModel>((componentName));
+           components.Append(newComponent);
+        }
+
+        return components;
     }
 }
